0e93e98de0f234b6e0d68b0e7a409088002665c5
kananelo <kananelo@kananelos-MacBook-Air.local>
2025-08-06 15:04:06 +0200

    feat: wire up borrower routes in App.js

--- 5klippa-web/src/App.js
+++ 5klippa-web/src/App.js
@@ -2,18 +2,22 @@
 
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage     from './pages/LoginPage';
-import RegisterPage  from './pages/RegisterPage';
-import DashboardPage from './pages/DashboardPage';
+import LoginPage               from './pages/LoginPage';
+import RegisterPage            from './pages/RegisterPage';
+import DashboardPage           from './pages/DashboardPage';
+import BorrowerDashboardPage   from './pages/BorrowerDashboardPage';
+import NewLoanPage             from './pages/NewLoanPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login"    element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard"element={<DashboardPage />} />
+        <Route path="/"                   element={<Navigate to="/login" replace />} />
+        <Route path="/login"              element={<LoginPage />} />
+        <Route path="/register"           element={<RegisterPage />} />
+        <Route path="/dashboard"          element={<DashboardPage />} />
+        <Route path="/borrower-dashboard" element={<BorrowerDashboardPage />} />
+        <Route path="/new-loan"           element={<NewLoanPage />} />
       </Routes>
     </BrowserRouter>
   );
